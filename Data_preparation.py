5b6ad8cdaecf92c3852b3459048adadd70f60622
Vijayadeepak <subramanian.a@outlook.com>
2018-03-16 16:55:32 -0500

    Input sampling

--- Data_preparation.py
+++ Data_preparation.py
@@ -1,7 +1,24 @@
 import csv
+import numpy
 
-txt_file = r"D:\class_work\560\project\data.txt"
-csv_file = r"D:\class_work\560\project\Input_data.csv"
-in_txt = csv.reader(open(txt_file, "rb"), delimiter = ' ')
+
+#print (os.getcwd())
+"""txt_file = '/Users/Sam/Downloads/data.txt'
+csv_file = '/Users/Sam/Downloads/Input_data.csv'
+in_txt = csv.reader(open(txt_file, 'rb'), delimiter = ' ')
 out_csv = csv.writer(open(csv_file, 'wb'))
-out_csv.writerows(in_txt)
+out_csv.writerows(in_txt)"""
+csv_file = '/Users/Sam/Downloads/Input_data.csv'
+csv_file_sample='/Users/Sam/Downloads/Input_sampled.csv'
+data = []
+with open (csv_file_sample,'wf') as wf:
+    with open(csv_file,'rb') as f:
+        csv_reader= csv.reader(f,delimiter=',')
+        for i,row in enumerate(csv_reader):
+            if i==1:
+                data1=(row[4])
+                data2=(row[5])
+                wf.write(data1)
+                wf.write(',')
+                wf.write(data2)
+                wf.write('\n')
